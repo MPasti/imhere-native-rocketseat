bb7056801b2047038b682a2ce486832c43721a45
MPasti <matheusspasti@gmail.com>
2024-03-28 16:00:16 -0300

    feat: adicionando novas funções e estado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from "expo-status-bar";
 import {
   Text,
   View,
   TextInput,
   TouchableOpacity,
   FlatList,
+  Alert,
 } from "react-native";
 import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
+import { useState } from "react";
 
 export default function Home() {
-  const participants = ["Roberto Carlos", "Tim Maia", "Michael Jackson"];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
 
-  function handleParticipantAdd() {}
+  function handleParticipantAdd() {
+    if (participants.includes(participantName))
+      return Alert.alert("Participante com esse nome já existe!");
 
-  function handleParticipantRemove() {}
+    //imutabilidade
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName("");
+  }
+
+  function handleParticipantRemove(name: string) {
+    Alert.alert("Remover", `Deseja remover o participante ${name}?`, [
+      {
+        text: "Sim",
+        onPress: () => {
+          setParticipants((prevState) =>
+            prevState.filter((item) => item !== name)
+          );
+          Alert.alert("Deletado!");
+        },
+      },
+      { text: "Não", style: "cancel" },
+    ]);
+  }
 
   return (
     <View style={styles.container}>
@@ -25,6 +47,8 @@ export default function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor={"#6B6B6B"}
+          value={participantName}
+          onChangeText={setParticipantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
@@ -38,12 +62,17 @@ export default function Home() {
           <Participant
             name={item}
             key={item}
-            onRemove={handleParticipantRemove}
+            onRemove={() => handleParticipantRemove(item)}
           />
         )}
+        showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmpty}>
+            Ninguém chegou no evento ainda? Adicione participantes à lista de
+            presença.
+          </Text>
+        )}
       />
-
-      <StatusBar style="auto" />
     </View>
   );
 }
